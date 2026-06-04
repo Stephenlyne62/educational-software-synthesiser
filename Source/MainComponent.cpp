da6def127b009e125f5cf56542f43c3925ddae6d
Stephen Lyne <slyne234@gmail.com>
2026-06-04 19:52:07 +0100

    Added audio output and first oscillator

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,27 +3,61 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
-    setSize (600, 400);
+    setSize(600, 400);
+
+    // 0 input channels, 2 output channels
+    setAudioChannels(0, 2);
 }
 
 MainComponent::~MainComponent()
 {
+    shutdownAudio();
 }
 
 //==============================================================================
-void MainComponent::paint (juce::Graphics& g)
+void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    currentSampleRate = sampleRate;
 
-    g.setFont (juce::FontOptions (16.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    auto frequency = 440.0; // A4 note
+    angleDelta = frequency * 2.0 * juce::MathConstants<double>::pi / currentSampleRate;
+}
+
+void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    auto* leftBuffer = bufferToFill.buffer->getWritePointer(0, bufferToFill.startSample);
+
+    auto* rightBuffer = bufferToFill.buffer->getNumChannels() > 1
+        ? bufferToFill.buffer->getWritePointer(1, bufferToFill.startSample)
+        : nullptr;
+
+    for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
+    {
+        auto currentSample = (float)std::sin(currentAngle) * 0.1f;
+
+        currentAngle += angleDelta;
+
+        leftBuffer[sample] = currentSample;
+
+        if (rightBuffer != nullptr)
+            rightBuffer[sample] = currentSample;
+    }
+}
+
+void MainComponent::releaseResources()
+{
+}
+
+//==============================================================================
+void MainComponent::paint(juce::Graphics& g)
+{
+    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+
+    g.setFont(juce::FontOptions(16.0f));
+    g.setColour(juce::Colours::white);
+    g.drawText("First JUCE Synth Tone - 440 Hz", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
 {
-    // This is called when the MainComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -7,7 +7,7 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::Component
+class MainComponent  : public juce::AudioAppComponent
 {
 public:
     //==============================================================================
@@ -17,10 +17,15 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
 
 private:
     //==============================================================================
-    // Your private member variables go here...
+    double currentSampleRate = 0.0;
+    double currentAngle = 0.0;
+    double angleDelta = 0.0;
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
